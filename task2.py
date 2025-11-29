9bea2832cda52dfc4463d0fff16f0db206aeb309
MEGHANANDH <g.meghanandh2016@gmail.com>
2025-11-29 12:04:18 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,8 @@
+
+def task_two():
+    print("Hello, welcome to Task 2!")
+    print("This is a simple Python program created for the internship assignment.")
+    print("Task 2 has been executed successfully.")
+
+
+task_two()
